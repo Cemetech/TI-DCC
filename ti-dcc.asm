cd81a9911756e107ddc312d319899d44284ddc29
geekboy1011 <geekboy1011@gmail.com>
2017-08-09 22:34:53 -0400

    Lots of changes. See file comments. Not working

--- ti-dcc.asm
+++ ti-dcc.asm
@@ -1,8 +1,52 @@
 .nolist
+.binarymode TI8X
 #include "ti84pcse.inc"
 #include "dcse8.inc"
-#include "include.inc"
+.module main
+.export
 
+
+IvtLocation	.equ	080h ; vector table at 8000h
+IsrLocation	.equ	85h ; ISR at TextShadPtrs
+
+periodA .equ 1 
+periodB .equ 1 << 1
+
+;.varloc appData,256 ; Used by IVT
+.varloc plotSScreen,768
+.varloc saveSScreen,768
+;.varloc statVars,531 ; Let us shoot to not use this, Also BCALL(_DelRes)
+.local
+.struct dccStateStruct      ;Digital Command Station State Vars
+    .var 2, msgPTR    ;pointer to current message being processed
+    .var 1, size     ;current bit "count" 
+    .var 1, curDat    ;Current shifted data byte
+    .var 1, flags     ;bit mask
+                      ; b0 = Output Link Mirror Bit
+                      ; b1 = Period A/B
+    .var 1, waitCTR   ;Set to 255 for pausing between packets ~15ms   
+    .var 1, repeat    ; 0-254 times, 255 until changed
+.endstruct 
+;Default initializations for queueing message
+; <ptr>
+; 0 
+; Preferabbly 0xFF
+; 0
+; 0 
+
+.struct dccPacketStruct   ;https://www.nmra.org/sites/default/files/s-9.2.1_2012_07.pdf
+    .var 1, size      ;number of data bits in this packet-1
+    .var 1, data1     ;packet bytes
+    .var 1, data2     ;
+    .var 1, data3     ;
+    .var 1, data4     ;
+    .var 1, data5     ;
+    .var 1, data6     ; error == address ^ data
+.endstruct
+.var dccStateStruct, dccstate
+.var dccPacketStruct, dccIdlePacket
+.var dccPacketStruct, dccUserPacket
+.endlocal
 .list
 
     .org UserMem
@@ -62,7 +106,7 @@ SetUpInterrupt:
     ld  (hl), a
     ldir
  
-    ld  hl, InterruptServiceRoutine
+    ld  hl, InterruptServiceRoutineEnter
     ld  de, IsrLocation*256+IsrLocation
     ld  bc, 3
     ldir
@@ -76,58 +120,177 @@ SetUpInterrupt:
 
     im 2
                             ;Testing initializers
+    
+    ld de,dataLLUT
+    call genLUT             ;set up lut for interrupts
+    
     ex af,af'
-    ld a,2
+    xor a
     ex af,af'
+    xor a
+    ld (dccState.flags),a
+    ld (dccState.size),a
+    ld hl,idlePacket
+    ld (dccState.msgPTR),hl
+    ld a,255
+    ld (dccState.waitCTR),a
+    ld (dccState.repeat),a
+    ld (dccState.curDat),a
     ei
 
     
     jp $
     
-
+genLUT:  ;DE=starting address. Must be 256-byte aligned.
+	ld a,e
+	rrca \ rrca \ rrca
+	and 7
+	ld (de),a
+	ld a,e
+	inc d
+	and 7
+	ld b,a
+	ld a,1
+	jr z,$+5
+	rlca
+	djnz $-1
+	ld (de),a
+	dec d
+	inc e
+	jr nz,genLUT
+	ret
+InterruptServiceRoutineEnter:
+    jp InterruptServiceRoutine
 InterruptServiceRoutine:
 
     ex af,af'               
         exx
             ld bc,(%00000011 << 8)+pCrstlTmr1Cfg ;
             out (c),b                ;17cc Faster then PushPop
-            or a                     ;If != Zero
+            
+            or a                     ;If != Zero.
             jp nz,doWait
+            
             ld a,(dccState.flags)
-            ;ld ix,(dccState.msgPTR)
-            out (0),a                ;No other flags then bit. 
+            out (0),a                ;Output bits to port 
             bit 1,a                  ;Check period
             ld a,(dccState.curDat)   ;Load A with current data being processed
             jp nz,doPeriodB          ;If Z We are period A   
 doPeriodA:
-            bit 0,a                  ;Check current data Bit
-            jp nz,+                  ;If Z set wait
+            or a                  ;Check current data Bit
+            jp nz,_notZeroBit1                 ;If Z set wait
             ld a,1  
             ld (dccState.waitCTR),a  ;Load Wait
-+:  
+_notZeroBit1:  
             ld a,periodB             ;Set period and Output Link Mirror Bit
             ld (dccState.flags),a    ;to %00000010
             jp doIntEnd
 doPeriodB:
-            bit 0,a                  ;Check current data bit
-            jp nz,+                  ;If Z set Wait
+            or a                     ;Check current data bit
+            jp nz,_notZeroBit2                  ;If Z set Wait
             ld a,1                   ;
             ld (dccState.waitCTR),a  ;Load Wait
-+:  
+_notZeroBit2:  
             ld a,periodA             ;Set period and Output Link Mirror Bit
-            ld (dccState.flags),a    ;to 0
-            jp doIntEnd              ;Need todo B specific stuff here
-doIntEnd:
+            ld (dccState.flags),a    ;to %00000001
+stateBEnd:
+.endasm
+            ;Ok time to load the next bit
+            ld hl,(dccState.msgPTR)    ;10
+            inc hl
+            ld a,(dccState.size)     ;13
+            ld b,a                   ;4
+            rrca \ rrca \ rrca       ;12
+            and %00011111            ;4   bitcount // 8
+            add a,l                  ;4   
+            ld l,a                   ;4   Offset HL by ^
+            ld a,b                   ;4  
+            and %00000111            ;4   bitcount % 8
+            ld b,a                   ;4   loop counter
+            ld a,(hl)                ;7   ld a with byte
+            jp z,_bitZero                   ;10  85
+_loop:          
+            rrca                     ;4 * b    w28
+            djnz _loop                   ;13 * b   w91  Shift to bit 1
+_bitZero:     
+            and 1
+.asm
+
+            ld hl,(dccState.msgPTR)
+            inc hl
+            ld a,(dccState.size)
+            ld e,a
+            ld d,dataLLUT>>8
+            ld a,(de) \	inc d  ;First LUT is for(i=0;i<256;i++) arr1[i] = (i>>3)&7;
+            add a,L
+            ld L,a
+            jr nc,$+3
+            inc h
+            ld a,(de)          ;Second LUT is for(i=0;i<256;i++) arr2[i] = 1<<(i&7);
+            and (hl)
+           
+            ld (dccState.curDat),a
+            
+            ld hl,(dccState.msgPTR)  
+            ld a,(dccState.size)
+            cp (hl)                     ;sub max from current
+            ;if zero
+            jp z,checkRepeat            ;If 0 we have sent the packet
+            inc a
+            ld (dccState.size),a        ;Store the size back for next call
+doIntEnd:   
+            ld a,(dccState.waitCTR)     ;waitCTR is set in loop or at last wait count
         exx
     ex af,af'
-    exx
     ei
     ret
-
+checkRepeat:                            ;If repeat !=0 repeat packet send
+            ld a,(dccState.repeat)
+            cp 255
+            jp z,repeatEntry            ;If repeat is 255 requeue
+            or a                        
+            jp loadIdleAfterRepeat      ;If repeat is zero load idle packet
+            dec a
+            ld (dccState.repeat),a      ;Else Dec and store repeat
+repeatEntry:
+            xor a
+            ld (dccState.size),a        ;reset packet
+            ld (dccState.flags),a
+            ld a,255
+            ld (dccState.waitCTR),a     ;Wait ~14ms for sanity
+            jp doWait                   ;no need to rest curdat is already 1
+            
+            
+loadIdleAfterRepeat:
+            xor a
+            ld (dccState.flags),a
+            ld (dccState.size),a
+            ld hl,idlePacket
+            ld (dccState.msgPTR),hl
+            ld a,255
+            ld (dccState.waitCTR),a
+            ld (dccState.repeat),a
+            ld (dccState.curDat),a
 doWait:
             dec a
+            ld (dccState.waitCTR),a ;Save wait
         exx
-    ex af',af
+    ex af,af'
+    ei
     ret
+.align 16
+idlePacket:
+.db 43
+.db %11111111
+.db %11111110
+.db %11111111
+.db %00000000
+.db %00111111
+.db %11100000
+.align 256
+dataLLut:
+.fill 512,0         ;I know this is bad should move this to saferam. But meh
+.endmodule
 .endrelocate
+.global
 .end
